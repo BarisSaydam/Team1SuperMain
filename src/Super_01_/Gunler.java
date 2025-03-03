22dd30e59462ebbbd64d8936eb4b7870f516e406
BarisSaydam <brssydm@gmail.com>
2025-03-03 12:58:37 -0500

    Haftanin ilk uc gunu ekllendi.

---
+++ src/Super_01_/Gunler.java
@@ -0,0 +1,9 @@
+package Super_01_;
+
+public class Gunler {
+    public static void main(String[] args) {
+        System.out.println("Pazartesi Sali Carsamba");
+        System.out.println("/*/*/*/");
+
+    }
+}
